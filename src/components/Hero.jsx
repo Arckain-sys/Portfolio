20bd55b31a822698490541ed0120ff57f0ed07f0
Abel Musa <abelmusa.m@gmail.com>
2024-05-20 16:54:07 -0400

    added some weight to hero font

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
               variants={container(0)}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+              className="pb-16 text-6xl font-extralight tracking-tight lg:mt-16 lg:text-8xl"
             >
               Abel Musa
             </motion.h1>
